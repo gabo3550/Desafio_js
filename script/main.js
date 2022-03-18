db857005972dee5ca9d54888875bf96986c08d3e
unknown <gabrielpodesta@gmail.com>
2022-03-18 13:44:15 -0300

    gabo3550

---
+++ script/main.js
@@ -0,0 +1,18 @@
+let tuTiempo = prompt ("Ingresa tu tiempo");
+let tiempoUsainBolt = 9.58;
+
+switch (true) {
+    case tuTiempo == 9.58:
+    console.log("Llegaste junto con Usain de la mano");
+    break;
+    case tuTiempo <9.58:
+    console.log("Le ganaste a Usain Bolt");
+    break;
+    case tuTiempo >9.58:
+    console.log("Esta vez perdiste con Usain Bolt");
+    break;
+    default:
+    console.log("Segui entrenando");
+    break;
+    }
+
